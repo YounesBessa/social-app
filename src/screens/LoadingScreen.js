9c6cc51af410f800848cda5a39ca0ca95ef47abc
Younès Bessa <bessa460@gmail.com>
2022-10-05 21:52:06 +0200

    🚀 final commit

---
+++ src/screens/LoadingScreen.js
@@ -0,0 +1,37 @@
+import React, { useEffect,useContext } from "react";
+import styled from "styled-components";
+import LottieView from "lottie-react-native";
+
+import {UserContext} from "../context/UserContext";
+
+import Text from "../components/Text";
+
+export default LoadingScreen = () => {
+  const [_, setUser] = useContext(UserContext);
+  useEffect(() => {
+    setTimeout(async () => {
+      setUser(state => ({...state, isLoggedIn: false}));
+    })
+  }, 500);
+  return (
+    <Container>
+      <Text title color="#FFFFFF">
+        SocialApp
+      </Text>
+
+      <LottieView
+        source={require("../../assets/loading.json")}
+        autoPlay
+        loop
+        style={{ width: "100%" }}
+      />
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #222222;
+`;
